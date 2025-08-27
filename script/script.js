14a1f575c652dbdf7d218361f943a6f86f29be1f
Rasel Parvez <raselpervezsocial@gmail.com>
2025-08-27 18:17:37 +0600

    Show an alert with a message including the service name and number and Each call will cut 20 coins. Reduce Coin after each click. done

--- script/script.js
+++ script/script.js
@@ -12,15 +12,39 @@ function funGetElByClass(id) {
 //Clicking on the 💗 heart icon of any card will increase the count in the Navbar
 
 // let heart-count value 0
-let  heartCount = 0;
+let heartCount = 0;
 
 // Select all heart buttons
 const heartButtons = document.querySelectorAll('.heart-button');
 
 heartButtons.forEach((heartButton) => {
     heartButton.addEventListener('click', function () {
-        heartCount ++
+        heartCount++
         funGetElById('heart-count').innerText = heartCount;
+    });
+});
+
+
+// Select all call-button
+const callButtonS = document.querySelectorAll('.call-button');
+
+// let coin-count value 0
+let coinCount = funGetElById('coin-count').innerText;
+console.log(coinCount);
+
+callButtonS.forEach((callButton) => {
+    callButton.addEventListener('click', function () {
+        const servicesName = callButton.parentNode.parentNode.childNodes[1].childNodes[3].innerText;
+
+        const servicesNumber = callButton.parentNode.parentNode.childNodes[1].childNodes[7].innerText;
+
+        //Show an alert with a message including the service name and number
+        alert(`📞 Calling ${servicesName} : ${servicesNumber}`);
+
+        //Each call will cut 20 coins. Reduce Coin after each click.
+        coinCount = Number(coinCount) - 20;
+        funGetElById('coin-count').innerText = coinCount;
+
     });
 });
 
